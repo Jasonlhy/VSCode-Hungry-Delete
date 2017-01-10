9dec1dcec00bb7053ebdbecb2dd5e7c9a97389e2
Jason Liu <j-lhy@hotmail.com>
2017-01-10 23:59:28 +0800

    Solve the bug and use a more robust algorithm

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,36 @@
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import {window, commands, ExtensionContext, Position} from 'vscode';
+import { window, commands, ExtensionContext, Position, Range, TextDocument, TextLine } from 'vscode';
+
+/**
+ * Back trace the first non-empty character position in the above line, used as start positon to be deleted
+ */
+function backtraceAboveLine(doc: TextDocument, cursorLineNumber: number): Position {
+    // backtrace the first non-empty character position
+    let backtraceLineNumber = cursorLineNumber - 1;
+    let empty = true;
+    while (backtraceLineNumber >= 0 && empty) {
+        empty = doc.lineAt(backtraceLineNumber).isEmptyOrWhitespace;
+        if (empty) {
+            backtraceLineNumber--;
+        }
+    }
+
+    let startPosition;
+    if (backtraceLineNumber < 0) {
+        startPosition = new Position(0, 0);
+    } else {
+        const nonEmptyLine = doc.lineAt(backtraceLineNumber);
+        startPosition = nonEmptyLine.range.end; // it is the one after the last character (which may be space)!
+    }
+
+    return startPosition;
+}
+
+function backtraceInLine(cursorLine: TextLine): Position {
+    return null;
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,32 +44,46 @@ export function activate(context: ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
+
+    // This extension simpliy override the keybinding ctrl+backspace to extends its hungry delete function above lines
+    // currently ctrl+backspace will hungry delete the entire whitespace on line same line before the cursor
+    // therefore, to implement "backtraceInLine" is not neccessary
     let disposable = commands.registerCommand('extension.hungryDelete', () => {
-        
-        // This extension simpliy override the keybinding ctrl+backspace
+        /* Edior and doc */
         const editor = window.activeTextEditor;
-        let position = editor.selection.active;
-        let currentLine = editor.document.lineAt(position);
+        const doc = editor.document;
 
-        // continue to eat all the whitespace
-        // until the line is not empty
-        let anyHungryDelete = false;
-        while (currentLine.isEmptyOrWhitespace && position.line > 0){
-            anyHungryDelete = true;
+        /* Cursor data */
+        const cursorPosition = editor.selection.active;
+        const cursorLineNumber = cursorPosition.line;
+        const cursorLine = doc.lineAt(cursorPosition);
 
-            commands.executeCommand("deleteLines", null, null);
-            commands.executeCommand("cursorUp", null, null);         
+        /* Find out the start position and end position to be deleted */
+        if (cursorLine.isEmptyOrWhitespace || (cursorPosition.character <= cursorLine.firstNonWhitespaceCharacterIndex)) {
+            const startPosition = backtraceAboveLine(doc, cursorLineNumber);
+            const endPosition = cursorPosition;
+            const deleteRange = new Range(startPosition, endPosition);
 
-            // It seems that editor.selection.active does not work
-            position = new Position(position.line - 1, 0);
-            currentLine = editor.document.lineAt(position);
-        } 
+            let result = editor.edit((editorBuilder) => {
+                // it includs the startPosition but exclude the endPositon
+                editorBuilder.delete(deleteRange);
+            })
 
-        if (anyHungryDelete){
-            commands.executeCommand("cursorEnd", null, null);
+            // only care Failed
+            result.then(function (success) {
+                if (!success) {
+                    window.showErrorMessage("Failed to delete the text");
+                }
+            }, function () {
+                window.showErrorMessage("Failed to invoke the editBuilder to delete the text");
+            })
         } else {
-            commands.executeCommand("deleteWordLeft", null, null);
-        }        
+            // delete whitespace on the same line only
+            const result = commands.executeCommand("deleteWordLeft", null, null);
+            result.then(null, function () {
+                window.showErrorMessage("Failed to delte word lft")
+            })
+        }
     });
 
     context.subscriptions.push(disposable);
